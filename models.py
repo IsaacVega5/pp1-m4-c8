ff3065c701fcc561069f9389a29fd6b181f6d1c9
IsaacVega <isaacvega361@gmail.com>
2024-12-16 20:15:03 -0300

    [feat] Create models

---
+++ models.py
@@ -0,0 +1,36 @@
+
+from typing import List
+from pydantic import BaseModel
+
+class BooksModel(BaseModel):
+  id : str
+  name: str
+  publisher: str
+  year: str
+  edition: int
+  authors: str
+  
+  books_lending : List["BookLendingModel"]
+  
+class UsersModel(BaseModel):
+  id : str
+  name: str
+  
+  users_lending : List["BookLendingModel"]
+
+class BookLendingModel(BaseModel):
+  book_id : str
+  user_id : str
+  start_date: str
+  end_date: str
+  
+  book : BooksModel
+  user : UsersModel
+
+class BookReviewModel(BaseModel):
+  id : str
+  book_id : str
+  review: str
+  validated: int
+  
+  book : BooksModel
